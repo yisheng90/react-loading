4b2cdb85175119c369e29b880e14b779271d5149
Yi <yisheng@yishenglee.com>
2020-05-31 14:02:37 +0800

    fix: 🐛 fix presisted red background on safari
    ✅ Closes: 4

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,28 @@
 export const isValidStyleColor = (color) => {
-    const style = new Option().style;
-    style.color = color;
-    return !!style.color
-}
+  const style = new Option().style;
+  style.color = color;
+  return !!style.color;
+};
+
+export const getTranslucent = (color) => {
+  const colorIndex = color.split(",");
+
+  return colorIndex.length === 4 ? parseFloat(colorIndex.pop() || "") : 1;
+};
+
+export const matchRgba = (color) =>
+  color.match(
+    /^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d*(?:\.\d+)?)\)$/
+  );
+
+export const getRgba = (color, opacity) => {
+  const match = matchRgba(color);
+
+  if (!match) {
+    return color;
+  }
+
+  return `rgba(${Number(match[1])}, ${Number(match[2])},  ${Number(
+    match[3]
+  )}, ${opacity !== undefined ? opacity : Number(match[4])})`;
+};
